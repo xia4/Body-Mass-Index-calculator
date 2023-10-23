9b486cc113cb857037faff6ddd6ee9f4e0b39a47
xia4 <77831217+xia4@users.noreply.github.com>
2023-10-23 17:29:48 +0900

    JS BMI 수치 계산 구현 & 두번 째, 세번 째 섹션 퍼블리싱

--- bmi-calculator/starter-code/index.js
+++ bmi-calculator/starter-code/index.js
@@ -9,7 +9,6 @@ function checkbox() {
       design.style.display = "none";
     }
   }
-  console.log(check_Box);
 }
 
 window.onload = () => {
@@ -24,27 +23,35 @@ function calculateBMI() {
   const height = parseInt(document.querySelector("#height").value);
   const weight = parseInt(document.querySelector("#weight").value);
   const result = document.querySelector("#result");
+  const bmiBox = document.querySelector(".bmi_box");
+  const bmiTxt = document.querySelector("#que")
+  const bmiTit = document.querySelector("#ans")
 
   if (height === "" || isNaN(height)) {
     /* height 값이 표현할 수 없는 값일 때 */
-    result.innerHTML = "키를 입력해 주세요!";
+    result.innerHTML = "";
+    
   } else if (weight === "" || isNaN(weight)) {
     /* weight 값이 표현할 수 없는 값일 때 */
-    result.innerHTML = "몸무게를 입력해 주세요!";
+    result.innerHTML = "";
+    bmiTxt.innerHTML = "Welcome!"
+    bmiTit.innerHTML = "Enter your height and weight and you’ll see your BMI result here"
+    // bmiTxt.style.fontSize = "24px"
+    bmiBox.classList.remove("active");
+    /* weight 값이 제거될 시 active 클래스 제거 */
   } else {
     /* height, weight 값이 모두 작성 될 때 */
     const bmi = (weight / ((height * height) / 10000)).toFixed(1);
     /* toFixed 메소드로 소수점 첫 번째 자리까지 반올림 */
-      if (bmi <= 18.5) {
-        result.innerHTML = `<span>${bmi}</span>`;
-      } else if (bmi <= 23) {
-        result.innerHTML = `<span>${bmi}</span>`;
-      } else if (bmi <= 25) {
-        result.innerHTML = `<span>${bmi}</span>`;
-      } else if (bmi <= 30) {
-        result.innerHTML = `<span>${bmi}</span>`;
-      } else {
-        result.innerHTML = `<span>${bmi}</span>`;
-      }
+    bmiBox.classList.add("active");
+    /* bmi 계산이 완료될 시 active 클래스 추가하여 레이아웃 변경 */
+    bmiTxt.innerHTML = "Your BMI is...";
+    /* bmi 계산이 완료될 시 text 변경 */
+    bmiTit.innerHTML = "Your BMI suggests you're a healthy weight. Tour ideal Weight is between 63.3kgs - 85.2kgs.";
+    /* bmi 계산이 완료될 시 text 변경 */
+    result.innerHTML = `<span>${bmi}</span>`
+    /* bmi 계산이 완료될 시 result 영역에 노출 */
   }
+  console.log(result)
 }
+
